cb737331865f8898e91d778f27926f8256a7b67e
teamwork469 <59301419+teamwork469@users.noreply.github.com>
2020-02-01 09:03:12 +0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+var koa = require('koa');
+var Router = require('koa-router');
+var app = koa();
+
+var router = new Router();
+
+
+app.use(_.routes()); //Use the routes defined using the router
+app.listen(3000);
